e6ec3f2b6f4380f122c39e665f70b73c5bcfe1d4
usm-manathunga <usmworks@gmail.com>
2025-08-06 15:59:31 +0530

    change cors

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, origins=["http://localhost:3000"])
     app.config.from_object(Config)
     db.init_app(app)
 
