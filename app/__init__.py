6955210fc08892f68116ce027e88773841ac8fda
usm-manathunga <usmworks@gmail.com>
2025-08-06 15:41:29 +0530

    enable cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from .extension import db
 from .config import Config
-
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
     db.init_app(app)
 
